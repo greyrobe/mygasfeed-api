9c20c77dc2c9ab3719b0b126980d7e53a56a255b
Adam Jaso <ajaso@sazze.com>
2014-04-14 12:53:27 -0700

    refactored to use foreman-api

--- src/main/java/com/ajaso/mygasfeed/api/MyGasFeedApi.java
+++ src/main/java/com/ajaso/mygasfeed/api/MyGasFeedApi.java
@@ -1,7 +1,13 @@
 package com.ajaso.mygasfeed.api;
 
-import com.ajaso.android.http.api.*;
-import com.ajaso.android.http.request.HttpRequest;
+
+import com.ajaso.android.foreman.api.annotation.ApiMethod;
+import com.ajaso.android.foreman.api.annotation.Host;
+import com.ajaso.android.foreman.api.annotation.UriParam;
+import com.ajaso.android.foreman.api.annotation.http.BodyParam;
+import com.ajaso.android.foreman.api.annotation.http.HttpMethod;
+import com.ajaso.android.foreman.api.http.HttpRequest;
+import com.ajaso.android.foreman.api.serializer.JsonSerializer;
 import com.ajaso.mygasfeed.api.response.*;
 
 /**
@@ -17,37 +23,40 @@ public interface MyGasFeedApi
     @UriParam
     public String apiKey = BuildConfig.DEBUG ? "rfej9napna" : "dk37p5ccr9";
 
-
-    @ApiMethod(uri="/locations/pricehistory/:stationId/:apiKey.json", response=History.class)
+    @HttpMethod
+    @ApiMethod(uri="/locations/pricehistory/:stationId/:apiKey.json", response=History.class, format=JsonSerializer.class)
     public HttpRequest getStationPriceHistory(@UriParam("stationId") int stationId);
 
 
-    @ApiMethod(uri="/station/details/:stationId/:apiKey.json", response=StationDetails.class)
+    @ApiMethod(uri="/station/details/:stationId/:apiKey.json", response=StationDetails.class, format=JsonSerializer.class)
     public HttpRequest getStation(@UriParam("stationId") int stationId);
 
-
-    @ApiMethod(uri="/stations/radius/:lat/:lon/:dist/:fuelType/:sortBy/:apiKey.json", response=Nearby.class)
+    @HttpMethod
+    @ApiMethod(uri="/stations/radius/:lat/:lon/:dist/:fuelType/:sortBy/:apiKey.json", response=Nearby.class, format=JsonSerializer.class)
     public HttpRequest getStationsNearby(
         @UriParam("lat") double lat, @UriParam("lon") double lon, @UriParam("dist") double dist,
         @UriParam("fuelType") String fuelType, @UriParam("sortBy") String sortBy);
 
 
-    @ApiMethod(uri="/stations/brands/:apiKey.json", response=Brands.class)
+    @HttpMethod
+    @ApiMethod(uri="/stations/brands/:apiKey.json", response=Brands.class, format=JsonSerializer.class)
     public HttpRequest getStationBrands();
 
 
-    @ApiMethod(method="POST", uri="/locations/price/:apiKey.json", response=PriceUpdate.class)
+    @HttpMethod(method="POST")
+    @ApiMethod(uri="/locations/price/:apiKey.json", response=PriceUpdate.class, format=JsonSerializer.class)
     public HttpRequest updateGasPrices(
         @UriParam("apiKey") String apiKey, @BodyParam("price") String price,
         @BodyParam("fueltype") String fueltype, @BodyParam("stationid") String stationid);
 
 
-    @ApiMethod(uri="/locations/geoaddress/:lat/:lon/:apiKey.json", response=AddressLocation.class)
+    @HttpMethod
+    @ApiMethod(uri="/locations/geoaddress/:lat/:lon/:apiKey.json", response=AddressLocation.class, format=JsonSerializer.class)
     public HttpRequest getAddressForLocation(
         @UriParam("lat") double lat, @UriParam("lon") double lon);
 
-
-    @ApiMethod(uri="/locations/otherclosebystations/:stationId/:limit/:apiKey.json", response=CloseBy.class)
+    @HttpMethod
+    @ApiMethod(uri="/locations/otherclosebystations/:stationId/:limit/:apiKey.json", response=CloseBy.class, format=JsonSerializer.class)
     public HttpRequest getOtherCloseByStations(
         @UriParam("stationId") int stationId, @UriParam("limit") int limit);
 
